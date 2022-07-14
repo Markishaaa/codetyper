844ea562a355aed570a435d9777281fb7eb276c0
unknown <marko.tanasic01@gmail.com>
2022-07-14 18:56:57 +0200

    added a db schema for user

---
+++ generate_db.sql
@@ -0,0 +1,7 @@
+create table user
+(
+    username      varchar(50) not null primary key,
+    role          enum('ADMIN', 'USER') not null,
+    password_hash tinytext    not null,
+    created_at    datetime    not null
+);
