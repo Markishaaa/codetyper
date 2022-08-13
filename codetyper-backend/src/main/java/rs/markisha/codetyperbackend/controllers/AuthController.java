46fc41f430c02da1c17cff9448a6e0b0a9231b54
marko <marko.tanasic01@gmail.com>
2022-08-13 11:50:30 +0200

    [be] login, logout and getting the current user

--- codetyper-backend/src/main/java/rs/markisha/codetyperbackend/controllers/AuthController.java
+++ codetyper-backend/src/main/java/rs/markisha/codetyperbackend/controllers/AuthController.java
@@ -4,18 +4,29 @@ import java.sql.Timestamp;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import rs.markisha.codetyperbackend.model.User;
 import rs.markisha.codetyperbackend.model.UserRole;
+import rs.markisha.codetyperbackend.payload.LoginRequest;
 import rs.markisha.codetyperbackend.payload.MessageResponse;
 import rs.markisha.codetyperbackend.payload.RegisterRequest;
 import rs.markisha.codetyperbackend.repositories.UserRepository;
+import rs.markisha.codetyperbackend.services.UserDetailsImpl;
+import rs.markisha.codetyperbackend.utils.JwtUtils;
 
 
 @RestController
@@ -26,9 +37,16 @@ public class AuthController {
 
 	private final PasswordEncoder passwordEncoder;
 
-	public AuthController(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+	private final AuthenticationManager authManager;
+
+	private final JwtUtils jwtUtils;
+
+	public AuthController(UserRepository userRepo, PasswordEncoder passwordEncoder, AuthenticationManager authManager,
+			JwtUtils jwtUtils) {
 		this.userRepo = userRepo;
 		this.passwordEncoder = passwordEncoder;
+		this.authManager = authManager;
+		this.jwtUtils = jwtUtils;
 	}
 
 	@PostMapping("/register")
@@ -51,11 +69,31 @@ public class AuthController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest request) {
+        if (!userRepo.existsByUsername(request.getUsername())) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Authentication auth = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+        ResponseCookie cookie = jwtUtils.generateJwtCookies(userDetails);
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutUser() {
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
+    }
+
 	@GetMapping("/getall")
 	public ResponseEntity<?> getall() {
 		return ResponseEntity.ok(userRepo.findAll());
-//		return ResponseEntity.ok().build();
 	}
-
-
 }
