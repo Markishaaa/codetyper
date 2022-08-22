3ebcf7d188a68b74141958a67bc3f9a94b5852c8
marko <marko.tanasic01@gmail.com>
2022-08-22 11:38:51 +0200

    [fe] added a timer

--- codetyper-frontend/src/app/app-routing.module.ts
+++ codetyper-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TypeCodeComponent } from './components/functionalities/type-code/type-code.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { RegisterComponent } from './components/user/register/register.component';
 
 const routes: Routes = [
+  {
+    path: "",
+    component: TypeCodeComponent
+  },
   {
     path: "register",
     component: RegisterComponent
--- codetyper-frontend/src/app/components/functionalities/type-code/type-code.component.ts
+++ codetyper-frontend/src/app/components/functionalities/type-code/type-code.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CodeSnippet } from 'src/app/api/code-snippet';
-import { GlobalConstants } from 'src/app/api/global-constants';
 import { CodeSnippetService } from 'src/app/services/code-snippet.service';
 import { SharedService } from 'src/app/services/shared.service';
 
@@ -17,6 +16,14 @@ export class TypeCodeComponent implements OnInit {
   getRandomSnippet: any;
   clickEventSubscription: Subscription | any;
 
+  timer: any;
+  stopTimer: any;
+  hour: string = "00";
+  minute: string = "00";
+  second: string = "00";
+
+  handleKeydown: any;
+
   constructor(private snippetService: CodeSnippetService, private sharedService: SharedService) { }
 
   ngOnInit(): void {
@@ -30,6 +37,52 @@ export class TypeCodeComponent implements OnInit {
     this.clickEventSubscription = this.sharedService.getClickEvent().subscribe(() => {
       this.getRandomSnippet();
     });
+
+    this.timer = () => {
+      let totalSeconds = 0;
+      let countTimer;
+      countTimer = () => {
+        ++totalSeconds;
+        let hour = Math.floor(totalSeconds / 3600);
+        let minute = Math.floor((totalSeconds - hour * 3600) / 60);
+        let second = totalSeconds - (hour * 3600 + minute * 60);
+
+        if (minute < 10 || second < 10) {
+          let formatNumber = (num: number): string => {
+            let formattedNumber = num.toLocaleString('en-US', {
+              minimumIntegerDigits: 2,
+              useGrouping: false
+            });
+
+            return formattedNumber;
+          }
+
+          this.hour = formatNumber(hour);
+          this.minute = formatNumber(minute);
+          this.second = formatNumber(second);
+        } else {
+          this.hour = hour.toString();
+          this.minute = minute.toString();
+          this.second = second.toString();
+        }
+      }
+
+      this.timer = setInterval(countTimer, 1000);
+    }
+
+    this.stopTimer = () => {
+      clearInterval(this.timer);
+    }
+
+    this.handleKeydown = (event: any) => {
+      if (event.key == "Tab") {
+        event.preventDefault();
+        let start = event.target.selectionStart;
+        let end = event.target.selectionEnd;
+        event.target.value = event.target.value.substring(0, start) + "\t" + event.target.value.substring(end);
+        event.target.selectionStart = event.target.selectionEnd = start + 1;
+      }
+    }
   }
 
 }
